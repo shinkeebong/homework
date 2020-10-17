deed01c0a8add13d79b9e74e4488b5e8d338a49d
shinkeebong <shinkeebong@naver.com>
2020-10-17 22:39:57 +0900

    Update week3_hw.py

--- week3_hw.py
+++ week3_hw.py
@@ -1,6 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
+from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 설치 먼저 해야겠죠?) <<< 제출후 수정한 부분입니다.
+
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta_genie  # 'dbsparta'라는 이름의 db를 사용합니다. 'dbsparta' db가 없다면 새로 만듭니다.
+
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 # 아래 빈 칸('')을 채워보세요
@@ -14,9 +19,9 @@ for tr in trs:
     title = tr.select_one('td.info > a.title.ellipsis').text.strip()
     artist = tr.select_one('td.info > a.artist.ellipsis').text
     print(rank, title, artist)
-
-# body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.title.ellipsis
-# body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.artist.ellipsis
+    # body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.number
+    # body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.title.ellipsis
+    # body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.artist.ellipsis
     doc = {  ## 이것도 하나의 틀로 가져가면 된다
         'rank': rank,
         'title': title,
